482cd023b0a307110393d7ad043af8d1deb446e3
Hai Pham <swiften.svc@gmail.com>
2019-04-16 00:14:43 +0800

    feat(facebook): add function to map platform request to generic requests

--- src/messenger/facebook-messenger.ts
+++ src/messenger/facebook-messenger.ts
@@ -1,4 +1,6 @@
 import { DeepReadonly } from 'ts-essentials';
+import { GenericRequest } from './generic-messenger';
+import { isType } from './utils';
 
 type BaseFacebookRequest = Readonly<{
   sender: Readonly<{ id: string }>;
@@ -47,3 +49,90 @@ export type FacebookUser = Readonly<{
   profile_pic?: string;
   id: string;
 }>;
+
+/**
+ * Map platform request to generic request for generic processing.
+ * @param webhook Facebook webhook data.
+ * @return An Array of generic request.
+ */
+export function mapPlatformRequest(webhook: FacebookWebhookRequest) {
+  const { object, entry } = webhook;
+
+  /**
+   * Group requests based on sender ID.
+   * @param reqs A request Array.
+   * @return A map of requests.
+   */
+  function groupRequests(reqs: FacebookRequest[]) {
+    const requestMap: { [K: string]: FacebookRequest[] } = {};
+
+    reqs.forEach(args => {
+      const senderID = args.sender.id;
+      requestMap[senderID] = (requestMap[senderID] || []).concat([args]);
+    });
+
+    return requestMap;
+  }
+
+  function processRequest(request: FacebookRequest): GenericRequest['data'] {
+    if (isType<FacebookRequest.Postback>(request, 'postback')) {
+      return [{ text: request.postback.payload }];
+    }
+
+    if (isType<FacebookRequest.Message>(request, 'message')) {
+      const { message } = request;
+
+      if (isType<FacebookRequest.Message.Text['message']>(message, 'text')) {
+        return [{ text: message.text }];
+      }
+
+      if (isType<FacebookRequest.Message.Attachment>(message, 'attachments')) {
+        const { attachments } = message;
+
+        return attachments.map(({ type, payload }) => {
+          if (
+            type === 'image' &&
+            isType<
+              FacebookRequest.Message.Attachment.Image['attachments'][0]['payload']
+            >(payload, 'url')
+          ) {
+            return { image_url: payload.url, text: payload.url };
+          }
+
+          throw Error(`FB: Unsupported payload: ${JSON.stringify(payload)}`);
+        });
+      }
+    }
+
+    throw Error(`FB: Unsupported messaging ${JSON.stringify(request)}`);
+  }
+
+  switch (object) {
+    case 'page':
+      if (entry !== undefined && entry !== null) {
+        const allRequests = entry
+          .map(({ messaging }) => messaging)
+          .filter(messaging => !!messaging)
+          .reduce((acc, requests) => acc.concat(requests));
+
+        const groupedRequests = groupRequests(allRequests);
+
+        return Object.entries(groupedRequests).map(
+          ([senderID, requests]: [string, FacebookRequest[]]) => ({
+            senderID,
+            oldContext: {},
+            data: requests
+              .map(req => processRequest(req))
+              .reduce((acc, items) => acc.concat(items), [])
+          })
+        );
+      }
+
+      break;
+
+    default:
+      break;
+  }
+
+  throw new Error(`FB: Invalid request: ${JSON.stringify(webhook)}`);
+}
---
+++ src/messenger/utils.ts
@@ -0,0 +1,13 @@
+/**
+ * Check if an object is of a certain type.
+ * @param object Any object.
+ * @param keys Possible object keys.
+ * @return A boolean value.
+ */
+export function isType<T, K extends keyof T = keyof T>(
+  object: any,
+  ...keys: K[]
+): object is T {
+  if (!object) return false;
+  return keys.every(key => object[key] !== undefined);
+}
