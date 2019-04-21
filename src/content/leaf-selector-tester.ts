558b8f46a23022b5f58bb0af590a211e55fb3bfd
Hai Pham <swiften.svc@gmail.com>
2019-04-21 17:41:21 +0800

    feat(leaf-selector): fix leaf selector tester

--- src/content/leaf-selector-tester.ts
+++ src/content/leaf-selector-tester.ts
@@ -1,191 +1,223 @@
-// import { deepClone, getCurrentLeafID, joinPaths } from '../common/utils';
-// import { Context } from '../type/common';
-// import { LeafSelector } from '../type/leaf-selector';
-// import {
-//   EnumeratedElement,
-//   LeafCombinationTesterParam
-// } from '../type/leaf-selector-tester';
+import { deepClone, getCurrentLeafID, joinPaths } from '../common/utils';
+import { Context } from '../type/common';
+import { LeafSelector } from '../type/leaf-selector';
+import {
+  EnumeratedElement,
+  LeafCombinationTesterParam
+} from '../type/leaf-selector-tester';
+import { GenericResponse } from '../type/messenger';
+import { ContentSubscription } from '../type/stream';
 
-// /**
-//  * Enumerate all combinations of values, for e.g., we have:
-//  * [1, 2] - [3, 4, 5]
-//  * Then all combinations are:
-//  * [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5]
-//  * @template T The type of value.
-//  * @param arrays An Array of Arrays of values.
-//  * @param callback The callback used to run through every combination.
-//  */
-// export function enumerateCombinationsWithCallback<T>(
-//   arrays: T[][],
-//   callback: (...elements: EnumeratedElement<T>[]) => void
-// ) {
-//   function enumerate<T>(
-//     arrays: T[][],
-//     currentArrayIndex: number,
-//     currentElements: EnumeratedElement<T>[],
-//     callback: (...elements: EnumeratedElement<T>[]) => void
-//   ) {
-//     if (currentElements.length === arrays.length) {
-//       callback(...currentElements);
-//       return;
-//     }
+/**
+ * Enumerate all combinations of values, for e.g., we have:
+ * [1, 2] - [3, 4, 5]
+ * Then all combinations are:
+ * [1, 3], [1, 4], [1, 5], [2, 3], [2, 4], [2, 5]
+ * @template T The type of value.
+ * @param arrays An Array of Arrays of values.
+ * @param callback The callback used to run through every combination.
+ */
+export function enumerateCombinationsWithCallback<T>(
+  arrays: T[][],
+  callback: (...elements: EnumeratedElement<T>[]) => void
+) {
+  function enumerate<T>(
+    arrays: T[][],
+    currentArrayIndex: number,
+    currentElements: EnumeratedElement<T>[],
+    callback: (...elements: EnumeratedElement<T>[]) => void
+  ) {
+    if (currentElements.length === arrays.length) {
+      callback(...currentElements);
+      return;
+    }
 
-//     (arrays[currentArrayIndex] || []).forEach((element, i) => {
-//       enumerate(
-//         arrays,
-//         currentArrayIndex + 1,
-//         [...currentElements, { index: i, value: element }],
-//         callback
-//       );
-//     });
-//   }
+    (arrays[currentArrayIndex] || []).forEach((element, i) => {
+      enumerate(
+        arrays,
+        currentArrayIndex + 1,
+        [...currentElements, { index: i, value: element }],
+        callback
+      );
+    });
+  }
 
-//   enumerate(arrays, 0, [], callback);
-// }
+  enumerate(arrays, 0, [], callback);
+}
 
-// /**
-//  * Enumerate all combinations of values, but this time return an Array of all
-//  * combinations.
-//  * @template T The type of value.
-//  * @param arrays An Array of Arrays of values.
-//  * @return An Array of all combinations.
-//  */
-// export function enumerateCombinations<T>(arrays: T[][]) {
-//   const nested: EnumeratedElement<T>[][] = [];
-//   enumerateCombinationsWithCallback(arrays, (...e) => nested.push(e));
-//   return nested;
-// }
+/**
+ * Enumerate all combinations of values, but this time return an Array of all
+ * combinations.
+ * @template T The type of value.
+ * @param arrays An Array of Arrays of values.
+ * @return An Array of all combinations.
+ */
+export function enumerateCombinations<T>(arrays: T[][]) {
+  const nested: EnumeratedElement<T>[][] = [];
+  enumerateCombinationsWithCallback(arrays, (...e) => nested.push(e));
+  return nested;
+}
 
-// /**
-//  * Create a leaf selector tester to sequentially test leaf flows.
-//  * @template C The context used by the current chatbot.
-//  * @param compareEqual Function to check equality of two objects.
-//  * @param defaultContext The default context object.
-//  * @param param2 A leaf selector instance.
-//  */
-// export function createLeafSelectorTester<C extends Context>(
-//   compareEqual: (v1: unknown, v2: unknown) => Promise<boolean | any>,
-//   defaultContext: C,
-//   { selectLeaf }: LeafSelector<C>
-// ) {
-//   type DefaultInputCombinationDependency = Readonly<{
-//     beforeAllCombinations?: () => Promise<void>;
-//     beforeEachCombination?: () => Promise<void>;
-//     afterEachCombination?: () => Promise<void>;
-//     afterAllCombinations?: () => Promise<void>;
-//     activeBranchPathsComponents: string[];
-//   }>;
+/**
+ * Create a leaf selector tester to sequentially test leaf flows.
+ * @template C The context used by the current chatbot.
+ * @param selector A leaf selector instance.
+ * @param defaultContext The default context object.
+ * @param senderID The sender ID.
+ * @param compareEqual Function to check equality of two objects.
+ */
+export function createLeafSelectorTester<C extends Context>(
+  selector: LeafSelector<C>,
+  defaultContext: C,
+  senderID: string,
+  compareEqual: (v1: unknown, v2: unknown) => Promise<boolean | any>
+) {
+  type DefaultInputCombinationDependency = Readonly<{
+    beforeAllCombinations?: () => Promise<void>;
+    beforeEachCombination?: () => Promise<void>;
+    afterEachCombination?: () => Promise<void>;
+    afterAllCombinations?: () => Promise<void>;
+    activeBranchPathsComponents: string[];
+  }>;
 
-//   type DefaultBranchSequenceDependency = Readonly<{
-//     beforeEachIteration?: () => Promise<void>;
-//     afterEachIteration?: () => Promise<void>;
-//   }>;
+  type DefaultBranchSequenceDependency = Readonly<{
+    beforeEachIteration?: () => Promise<void>;
+    afterEachIteration?: () => Promise<void>;
+  }>;
 
-//   return {
-//     defaultContext,
-//     defaultInputCombinationDependency: (
-//       params: DefaultInputCombinationDependency
-//     ): DefaultInputCombinationDependency => params,
+  return {
+    defaultContext,
+    defaultInputCombinationDependency: (
+      params: DefaultInputCombinationDependency
+    ): DefaultInputCombinationDependency => params,
 
-//     testInputCombinations: async <Leaves>(
-//       {
-//         beforeAllCombinations,
-//         beforeEachCombination,
-//         afterEachCombination,
-//         afterAllCombinations,
-//         activeBranchPathsComponents
-//       }: DefaultInputCombinationDependency,
-//       leaves: LeafCombinationTesterParam<C, Leaves>[]
-//     ) => {
-//       const textInputs = leaves.map(({ possibleInputs }) => possibleInputs);
-//       const textCombinations = enumerateCombinations(textInputs);
-//       if (beforeAllCombinations) await beforeAllCombinations();
+    testInputCombinations: async <Leaves>(
+      {
+        beforeAllCombinations,
+        beforeEachCombination,
+        afterEachCombination,
+        afterAllCombinations,
+        activeBranchPathsComponents
+      }: DefaultInputCombinationDependency,
+      leaves: LeafCombinationTesterParam<C, Leaves>[]
+    ) => {
+      const textInputs = leaves.map(({ possibleInputs }) => possibleInputs);
+      const textCombinations = enumerateCombinations(textInputs);
+      if (beforeAllCombinations) await beforeAllCombinations();
 
-//       const results = await Promise.all(
-//         textCombinations.map(async combinations => {
-//           if (beforeEachCombination) await beforeEachCombination();
-//           let oldContext = defaultContext;
+      const results = await Promise.all(
+        textCombinations.map(async combinations => {
+          if (beforeEachCombination) await beforeEachCombination();
+          let oldContext = defaultContext;
 
-//           const result = await Promise.all(
-//             combinations.map(async ({ index: inputIndex, value }, i) => {
-//               const {
-//                 beforeStory,
-//                 afterStory,
-//                 leafKey,
-//                 checkExternals,
-//                 expectedContext
-//               } = leaves[i];
+          const result = await Promise.all(
+            combinations.map(async ({ index: inputIndex, value: text }, i) => {
+              const {
+                beforeStory,
+                afterStory,
+                leafKey,
+                checkExternals,
+                expectedContext
+              } = leaves[i];
 
-//               if (beforeStory) await beforeStory();
-//               let { newContext } = await selectLeaf(oldContext, value);
-//               const { activeBranch, ...restContext } = newContext;
+              if (beforeStory) await beforeStory();
 
-//               newContext = {
-//                 ...deepClone(restContext),
-//                 activeBranch: undefined
-//               } as C;
+              let { newContext } = await new Promise<GenericResponse<C>>(
+                async resolve => {
+                  await selector.next({ senderID, oldContext, text });
+                  let subscription: ContentSubscription;
 
-//               if (expectedContext) {
-//                 const incomingContext = deepClone(expectedContext(inputIndex));
+                  subscription = await selector.subscribe({
+                    next: async content => {
+                      resolve(content);
+                      !!subscription && (await subscription.unsubscribe());
+                    }
+                  });
 
-//                 if (!(await compareEqual(incomingContext, newContext))) {
-//                   throw Error(
-//                     `Mismatched context:
-//                       - Exp: ${JSON.stringify(incomingContext)}\n\n
-//                       - Got: ${JSON.stringify(newContext)}.
-//                       FYI, input was ${value} for story ${leafKey}`
-//                   );
-//                 }
-//               }
+                  !!subscription && (await subscription.unsubscribe());
+                }
+              );
 
-//               if (checkExternals) await checkExternals(newContext);
+              const { activeBranch, ...restContext } = newContext;
+              newContext = { ...deepClone(restContext) } as C;
 
-//               if (joinPaths(...activeBranchPathsComponents) !== activeBranch) {
-//                 throw Error(
-//                   `Mismatched active flow:
-//                     - Exp: ${joinPaths(...activeBranchPathsComponents)}
-//                     - Got: ${activeBranch}`
-//                 );
-//               }
+              if (expectedContext) {
+                const incomingContext = deepClone(expectedContext(inputIndex));
 
-//               oldContext = newContext;
-//               if (afterStory) await afterStory();
-//               return undefined;
-//             })
-//           );
+                if (!(await compareEqual(incomingContext, newContext))) {
+                  throw Error(
+                    `Mismatched context:
+                      - Exp: ${JSON.stringify(incomingContext)}\n\n
+                      - Got: ${JSON.stringify(newContext)}.
+                      FYI, input was ${text} for story ${leafKey}`
+                  );
+                }
+              }
 
-//           if (afterEachCombination) await afterEachCombination();
-//           return result;
-//         })
-//       );
+              if (checkExternals) await checkExternals(newContext);
 
-//       if (afterAllCombinations) await afterAllCombinations();
-//       return results;
-//     },
-//     testStorySequence: async <Leaves>(
-//       {
-//         beforeEachIteration,
-//         afterEachIteration
-//       }: DefaultBranchSequenceDependency,
-//       sequence: Readonly<{ text: string; leafID: keyof Leaves }>[]
-//     ) => {
-//       let oldContext = defaultContext;
+              if (joinPaths(...activeBranchPathsComponents) !== activeBranch) {
+                throw Error(
+                  `Mismatched active flow:
+                    - Exp: ${joinPaths(...activeBranchPathsComponents)}
+                    - Got: ${activeBranch}`
+                );
+              }
 
-//       return Promise.all(
-//         sequence.map(async ({ text, leafID }, i) => {
-//           if (beforeEachIteration) await beforeEachIteration();
-//           const { newContext } = await selectLeaf(oldContext, text);
-//           const currentLeafID = getCurrentLeafID(newContext.activeBranch);
+              oldContext = newContext;
+              if (afterStory) await afterStory();
+              return undefined;
+            })
+          );
 
-//           if (leafID !== currentLeafID) {
-//             throw Error(`${i}: Expected ${currentLeafID}, got ${leafID}`);
-//           }
+          if (afterEachCombination) await afterEachCombination();
+          return result;
+        })
+      );
 
-//           oldContext = newContext;
-//           if (afterEachIteration) await afterEachIteration();
-//           return undefined;
-//         })
-//       );
-//     }
-//   };
-// }
+      if (afterAllCombinations) await afterAllCombinations();
+      return results;
+    },
+    testStorySequence: async <Leaves>(
+      {
+        beforeEachIteration,
+        afterEachIteration
+      }: DefaultBranchSequenceDependency,
+      sequence: Readonly<{ text: string; leafID: keyof Leaves }>[]
+    ) => {
+      let oldContext = defaultContext;
+
+      return Promise.all(
+        sequence.map(async ({ text, leafID }, i) => {
+          if (beforeEachIteration) await beforeEachIteration();
+
+          const { newContext } = await new Promise<GenericResponse<C>>(
+            async resolve => {
+              await selector.next({ senderID, oldContext, text });
+              let subscription: ContentSubscription;
+
+              subscription = await selector.subscribe({
+                next: async content => {
+                  resolve(content);
+                  !!subscription && (await subscription.unsubscribe());
+                }
+              });
+
+              !!subscription && (await subscription.unsubscribe());
+            }
+          );
+
+          const currentLeafID = getCurrentLeafID(newContext.activeBranch);
+
+          if (leafID !== currentLeafID) {
+            throw Error(`${i}: Expected ${currentLeafID}, got ${leafID}`);
+          }
+
+          oldContext = newContext;
+          if (afterEachIteration) await afterEachIteration();
+          return undefined;
+        })
+      );
+    }
+  };
+}
--- test/content/leaf-selector-tester.test.ts
+++ test/content/leaf-selector-tester.test.ts
@@ -1,64 +1,64 @@
-// import { BigNumber } from 'bignumber.js';
-// import expectJs from 'expect.js';
-// import { describe, it } from 'mocha';
-// import { enumerateCombinations } from '../../src/content/leaf-selector-tester';
+import { BigNumber } from 'bignumber.js';
+import expectJs from 'expect.js';
+import { describe, it } from 'mocha';
+import { enumerateCombinations } from '../../src/content/leaf-selector-tester';
 
-// describe('Leaf selector tester utilities', () => {
-//   function range(length: number) {
-//     return [...Array(length).keys()];
-//   }
+describe('Leaf selector tester utilities', () => {
+  function range(length: number) {
+    return [...Array(length).keys()];
+  }
 
-//   function randomBetween(incl: number, excl: number) {
-//     return Math.round((excl - incl) * Math.random()) + incl;
-//   }
+  function randomBetween(incl: number, excl: number) {
+    return Math.round((excl - incl) * Math.random()) + incl;
+  }
 
-//   it('Enumerating combinations should work', () => {
-//     /// Setup
-//     const testData = range(4).map(() =>
-//       range(randomBetween(3, 6)).map(() =>
-//         range(randomBetween(3, 6)).map(() => randomBetween(1, 100))
-//       )
-//     );
+  it('Enumerating combinations should work', () => {
+    /// Setup
+    const testData = range(4).map(() =>
+      range(randomBetween(3, 6)).map(() =>
+        range(randomBetween(3, 6)).map(() => randomBetween(1, 100))
+      )
+    );
 
-//     function combinationSum(combinations: number[][]) {
-//       let current = new BigNumber(1);
+    function combinationSum(combinations: number[][]) {
+      let current = new BigNumber(1);
 
-//       combinations.forEach(combination =>
-//         combination.forEach(element => (current = current.plus(element)))
-//       );
+      combinations.forEach(combination =>
+        combination.forEach(element => (current = current.plus(element)))
+      );
 
-//       return current;
-//     }
+      return current;
+    }
 
-//     function combinationCount(data: number[][]) {
-//       let current = 1;
-//       data.forEach(({ length }) => (current *= length));
-//       return current;
-//     }
+    function combinationCount(data: number[][]) {
+      let current = 1;
+      data.forEach(({ length }) => (current *= length));
+      return current;
+    }
 
-//     function totalRawDataSum(data: number[][]) {
-//       let current = new BigNumber(1);
+    function totalRawDataSum(data: number[][]) {
+      let current = new BigNumber(1);
 
-//       data.forEach((datum, i) => {
-//         const dataCopy = data.map(datum1 => datum1);
-//         dataCopy.splice(i, 1);
-//         const combiCount = combinationCount(dataCopy);
-//         datum.forEach(elem => (current = current.plus(elem * combiCount)));
-//       });
+      data.forEach((datum, i) => {
+        const dataCopy = data.map(datum1 => datum1);
+        dataCopy.splice(i, 1);
+        const combiCount = combinationCount(dataCopy);
+        datum.forEach(elem => (current = current.plus(elem * combiCount)));
+      });
 
-//       return current;
-//     }
+      return current;
+    }
 
-//     /// When
-//     testData.forEach(data => {
-//       const allCombinations = enumerateCombinations(data);
-//       expectJs(allCombinations.length).to.equal(combinationCount(data));
+    /// When
+    testData.forEach(data => {
+      const allCombinations = enumerateCombinations(data);
+      expectJs(allCombinations.length).to.equal(combinationCount(data));
 
-//       expectJs(
-//         combinationSum(allCombinations.map(cs => cs.map(({ value }) => value)))
-//           .minus(totalRawDataSum(data))
-//           .toNumber()
-//       ).to.equal(0);
-//     });
-//   });
-// });
+      expectJs(
+        combinationSum(allCombinations.map(cs => cs.map(({ value }) => value)))
+          .minus(totalRawDataSum(data))
+          .toNumber()
+      ).to.equal(0);
+    });
+  });
+});
