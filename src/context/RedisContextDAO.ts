28e154321580e9a1c6040b7eb2bed50ffba76b82
Hai Pham <swiften.svc@gmail.com>
2019-04-24 02:43:15 +0800

    feat(context-dao): add Redix context DAO implementation

---
+++ src/context/RedisContextDAO.ts
@@ -0,0 +1,33 @@
+import { RedisClient } from 'redis';
+import { promisify1, promisify2 } from '../common/utils';
+import { Context } from '../type/common';
+import { ContextDAO } from '../type/context-dao';
+import { SupportedPlatform } from '../type/messenger';
+
+export function createRedisContextDAO<C extends Context>(
+  redis: RedisClient,
+  platform: SupportedPlatform
+): ContextDAO<C> {
+  function getCacheKey(senderID: string) {
+    return `${platform}-${senderID}`;
+  }
+
+  const promisifiedGet = promisify1(redis.get).bind(redis);
+  const promisifiedSet = promisify2(redis.set).bind(redis);
+  const promisifiedDel = promisify1(redis.del).bind(redis);
+
+  const contextDAO: ContextDAO<C> = {
+    getContext: async senderID => {
+      const context = await promisifiedGet(getCacheKey(senderID));
+      return JSON.parse(context);
+    },
+    setContext: (senderID, context) => {
+      return promisifiedSet(getCacheKey(senderID), JSON.stringify(context));
+    },
+    resetContext: senderID => {
+      return promisifiedDel(getCacheKey(senderID));
+    }
+  };
+
+  return contextDAO;
+}
