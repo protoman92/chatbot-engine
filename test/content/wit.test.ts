4e8aea3fc97f0fce199f0fbb17900f6af81fc111
Hai Pham <swiften.svc@gmail.com>
2019-05-09 13:12:39 +0800

    test(wit): add tests for wit higher order transformer and communicator

---
+++ test/content/wit.test.ts
@@ -0,0 +1,79 @@
+import { beforeEach, describe, it } from 'mocha';
+import { anything, deepEqual, instance, spy, verify, when } from 'ts-mockito';
+import { useWitEngine } from '../../src/content/higher-order/wit';
+import { Leaf } from '../../src/type/leaf';
+import { WitCommunicator, WitContext, WitResponse } from '../../src/type/wit';
+
+const senderID = 'sender-id';
+
+describe('Wit higher order function', () => {
+  let comm: WitCommunicator;
+  let rootLeaf: Leaf<WitContext>;
+
+  beforeEach(() => {
+    comm = spy<WitCommunicator>({ validate: () => Promise.reject('') });
+
+    rootLeaf = spy<Leaf<WitContext>>({
+      next: () => Promise.reject(''),
+      subscribe: () => Promise.reject('')
+    });
+  });
+
+  it('Wit engine should not fire if no error', async () => {
+    // Setup
+    when(rootLeaf.next(anything())).thenResolve({});
+    const transformed = useWitEngine(instance(comm))(instance(rootLeaf));
+
+    // When
+    const input = {
+      senderID,
+      inputText: 'some-text',
+      inputImageURL: undefined,
+      inputCoordinate: undefined
+    };
+
+    await transformed.next(input);
+
+    // Then
+    verify(comm.validate(anything())).never();
+  });
+
+  it('Wit engine should intercept errors', async () => {
+    // Setup
+    const witEntities: WitResponse['entities'] = {
+      a: [{ confidence: 1, value: 'some-value', type: 'value' }]
+    };
+
+    const inputText = 'some-text';
+
+    when(rootLeaf.next(anything())).thenCall(async ({ witEntities = {} }) => {
+      if (Object.entries(witEntities).length === 0) {
+        throw new Error('some-error');
+      }
+
+      return {};
+    });
+
+    when(comm.validate(anything())).thenResolve({
+      entities: witEntities,
+      _text: inputText,
+      msg_id: ''
+    });
+
+    const transformed = useWitEngine(instance(comm))(instance(rootLeaf));
+
+    // When
+    const input = {
+      senderID,
+      inputText,
+      inputImageURL: undefined,
+      inputCoordinate: undefined
+    };
+
+    await transformed.next(input);
+
+    // Then
+    verify(comm.validate(inputText)).once();
+    verify(rootLeaf.next(deepEqual({ ...input, witEntities }))).once();
+  });
+});
---
+++ test/mesenger/wit-communicator.ts
@@ -0,0 +1,43 @@
+import { beforeEach, describe, it } from 'mocha';
+import { anything, deepEqual, instance, spy, verify, when } from 'ts-mockito';
+import { createWitCommunicator } from '../../src/messenger/wit-communicator';
+import { HTTPCommunicator } from '../../src/type/communicator';
+import { WitCommunicator, WitConfigs } from '../../src/type/wit';
+
+describe('Wit communicator', () => {
+  let comm: HTTPCommunicator;
+  let witConfig: WitConfigs;
+  let witCommunicator: WitCommunicator;
+
+  beforeEach(() => {
+    comm = spy<HTTPCommunicator>({ communicate: () => Promise.reject('') });
+    witConfig = spy<WitConfigs>({ authorizationToken: '' });
+
+    witCommunicator = createWitCommunicator(
+      instance(comm),
+      instance(witConfig)
+    );
+  });
+
+  it('Should communicate with wit API correctly', async () => {
+    // Setup
+    const authorizationToken = 'some-auth-token';
+    when(comm.communicate(anything())).thenResolve({});
+    when(witConfig.authorizationToken).thenReturn(authorizationToken);
+
+    // When
+    const message = 'some-message';
+    await witCommunicator.validate(message);
+
+    // Then
+    verify(
+      comm.communicate(
+        deepEqual({
+          method: 'GET',
+          url: `https://api.wit.ai/message?q=${message}`,
+          headers: { Authorization: `Bearer ${authorizationToken}` }
+        })
+      )
+    ).once();
+  });
+});
