b87b79fafd05e958871f9394e3f9a53a160d86f2
Hai Pham <swiften.svc@gmail.com>
2019-05-17 03:07:01 +0800

    feat(facebook): add FacebookVisualContent

--- src/type/facebook-visual-content.d.ts
+++ src/type/facebook-visual-content.d.ts
@@ -1,4 +1,9 @@
-import { QuickReply, Response } from './visual-content';
+import { QuickReply, Response, VisualContent } from './visual-content';
 
 export type FacebookQuickReply = QuickReply;
 export type FacebookResponse = Response;
+
+export interface FacebookVisualContent extends VisualContent {
+  readonly quickReplies?: readonly FacebookQuickReply[];
+  readonly response: FacebookResponse;
+}
--- src/type/response.d.ts
+++ src/type/response.d.ts
@@ -1,8 +1,21 @@
+import { FacebookVisualContent } from './facebook-visual-content';
 import { SupportedPlatform } from './messenger';
 import { VisualContent } from './visual-content';
 
 /** A platform-specific response. */
 export type PlatformResponse = unknown;
+
+interface BaseGenericResponse<C> {
+  readonly senderID: string;
+  readonly additionalContext?: Partial<C>;
+}
+
+export namespace GenericResponse {
+  export interface Facebook<C> extends BaseGenericResponse<C> {
+    readonly senderPlatform: 'facebook';
+    readonly visualContents: readonly FacebookVisualContent[];
+  }
+}
 
 /**
  * A generic outgoing response. We can specify additional context to add to
@@ -10,9 +23,4 @@ export type PlatformResponse = unknown;
  * This is to prevent stale old context replacing latest one.
  * @template C The context used by the current chatbot.
  */
-export interface GenericResponse<C> {
-  readonly senderID: string;
-  readonly senderPlatform: SupportedPlatform;
-  readonly additionalContext?: Partial<C>;
-  readonly visualContents: readonly VisualContent[];
-}
+export type GenericResponse<C> = GenericResponse.Facebook<C>;
