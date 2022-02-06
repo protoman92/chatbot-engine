117d5b2cb761687ed144a24ea6b3954930a4445b
Hai Pham <swiften.svc@gmail.com>
2022-02-06 22:56:04 +0800

    feat(microbackend-plugin-express): add currentProjectDir to list of plugins for branch extension

--- package/microbackend-plugin-express/webpack.build.ts
+++ package/microbackend-plugin-express/webpack.build.ts
@@ -1,18 +1,20 @@
 import { mergeConfigs } from "@microbackend/plugin-core/build/utils.build";
 import { createMicrobackendExtension } from "@microbackend/plugin-core/build/webpack.build";
 import path from "path";
-import { TsconfigPathsPlugin } from "tsconfig-paths-webpack-plugin";
 import { Configuration } from "webpack";
 import type { IPluginOptions } from "./src";
 
-export default ({ allPlugins, environment }: IPluginOptions): Configuration => {
+export default ({
+  allPlugins,
+  currentProjectDir,
+  environment,
+}: IPluginOptions): Configuration => {
   return mergeConfigs(
     createMicrobackendExtension({
       environment,
       dirname: __dirname,
       extensionSubpath: path.join("chatbot_engine", "branch"),
-      plugins: allPlugins,
-    }),
-    { resolve: { plugins: [new TsconfigPathsPlugin()] } }
+      plugins: [...allPlugins, currentProjectDir],
+    })
   );
 };
