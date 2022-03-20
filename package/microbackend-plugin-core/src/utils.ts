37899f6811df1e0576f155817313ceec34d93f01
Hai Pham <swiften.svc@gmail.com>
2022-03-20 13:43:44 +0800

    feat(microbackend-plugin-core): fix import issues for error leaf arguments

--- package/microbackend-plugin-core/src/utils.ts
+++ package/microbackend-plugin-core/src/utils.ts
@@ -1,21 +1,21 @@
-import { ErrorLeafTrackErrorArgs } from "@haipham/chatbot-engine-core";
+import { _ErrorLeafConfig } from "@haipham/chatbot-engine-core";
 
 export const PLUGIN_NAME = "@microbackend/plugin-chatbot-engine-core";
 
 export class LeafHandlingError
   extends Error
-  implements Omit<ErrorLeafTrackErrorArgs, "error">
+  implements Omit<_ErrorLeafConfig.TrackErrorArgs, "error">
 {
-  erroredLeaf: ErrorLeafTrackErrorArgs["erroredLeaf"];
-  targetID: ErrorLeafTrackErrorArgs["targetID"];
-  targetPlatform: ErrorLeafTrackErrorArgs["targetPlatform"];
+  erroredLeaf: _ErrorLeafConfig.TrackErrorArgs["erroredLeaf"];
+  targetID: _ErrorLeafConfig.TrackErrorArgs["targetID"];
+  targetPlatform: _ErrorLeafConfig.TrackErrorArgs["targetPlatform"];
 
   constructor({
     error,
     erroredLeaf,
     targetID,
     targetPlatform,
-  }: ErrorLeafTrackErrorArgs) {
+  }: _ErrorLeafConfig.TrackErrorArgs) {
     super(error.message);
     this.stack = error.stack;
     this.erroredLeaf = erroredLeaf;
